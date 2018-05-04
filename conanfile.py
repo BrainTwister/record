c50ba2066b15166cae48e4d6caa13b6b3e4def9e
Bernd Doser <bernd.doser@braintwister.eu>
2018-05-04 18:05:11 +0000

    update conanfile

--- conanfile.py
+++ conanfile.py
@@ -4,8 +4,12 @@ class RecordConan(ConanFile):
     
     name = "record"
     version = "1.0"
+    license = "MIT"
+    description = "A heterogeneous data structure for C++"
+    homepage = "https://braintwister.eu"
+    url = "https://github.com/braintwister/record.git"
     
-    exports_sources = "include/*"
+    exports_sources = "include/*", "test/*", "CMakeLists.txt"
     no_copy_source = True
     
     settings = "os", "compiler", "build_type", "arch"
